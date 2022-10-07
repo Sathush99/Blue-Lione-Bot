752030b5bff1d9ef62b269aa04e6642b71cb933e
Sathush99 <115262717+Sathush99@users.noreply.github.com>
2022-10-07 22:51:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,27 +54,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['94714188730'] //ur owner number
-global.ownername = "nipuna rangana" //ur owner name
-global.ytname = "YT: 𝐂𝐈𝐁𝐄𝐑 𝐓𝐄𝐂𝐇" //ur yt chanel name
-global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['🐉 +𝟵𝟰𝟳𝟴𝟮𝟳𝟱𝟯𝟯𝟵𝟲'] //ur owner number
+global.ownername = "🐉 𝗦𝗔𝗧𝗛𝗨𝗦𝗛 𝗔𝗠𝗔𝗡𝗧𝗛𝗔" //ur owner name
+global.ytname = "YT: 🐉 𝗞𝗲𝗻𝗭𝗼" //ur yt chanel name
+global.socialm = "GitHub: 𝗦𝗮𝘁𝗵𝘂𝘀𝗵𝟵𝟵 //ur github or insta name
+global.location = "🐉 𝗦𝗿𝗶 𝗟𝗮𝗻𝗸𝗮,𝗠𝗔𝗧𝗔𝗥𝗔 " //ur location
 
 //bot bomdy
 global.owner = ['94714188730']
 global.ownertag = '94714188730' //ur tag number
-global.botname = '📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶' //ur bot name
+global.botname = '🐉 𝗥𝗘𝗗 𝗗𝗥𝗔𝗚𝗢𝗡 🐉' //ur bot name
 global.linkz = "https://youtu.be/3J9-8U8St3U" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/3J9-8U8St3U" //ur website to be displayed
 global.botscript = 'https://github.com/Nipuna-apps/Blue-Lione-Bot' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶" //ur sticker watermark packname
-global.author = "📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶" //ur sticker watermark author
+global.packname = "🐉 𝗥𝗘𝗗 𝗗𝗥𝗔𝗚𝗢𝗡 🐉 " //ur sticker watermark packname
+global.author = " '🐉 𝗥𝗘𝗗 𝗗𝗥𝗔𝗚𝗢𝗡 🐉 " //ur sticker watermark author
 global.menumoji ="💖"
 global.menuimg = "https://i.ibb.co/0mDLtyr/f2337149532299-58b7611d91faf.jpg" //ur alive img link [https://i.ibb.co]
 global.alive = "https://raw.githubusercontent.com/Nipuna-apps/Blue-Lion-Bot-V2/main/LioneMedia/video/Lione-bot.mp4" //ur alive video or image link [github.com]
-global.alivemg="Hi i am 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 bot.create by nipuna rangana." //ur alive masage
+global.alivemg="Hi i am '🐉 𝗥𝗘𝗗 𝗗𝗥𝗔𝗚𝗢𝗡 🐉 bot.create by 𝗦𝗔𝗧𝗛𝗨𝗦𝗛 𝗔𝗠𝗔𝗡𝗧𝗛𝗔 ." //ur alive masage
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
